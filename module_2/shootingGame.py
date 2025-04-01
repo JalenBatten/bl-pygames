f4b309c1829c7ef7ace0eaa6ecc098d7bc937392
JalenBatten <jbatten396@boyslatin.net>
2025-04-01 09:09:53 -0400

    Shooting Game

--- module_2/shootingGame.py
+++ module_2/shootingGame.py
@@ -1,57 +1,88 @@
-import pygame 
+import pygame
 import random
 
 pygame.init()
 
-screen= pygame.display.set_mode((800,600))
+FPS = 60
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Shooting Game")
 
-class Player():
+class Player(pygame.sprite.Sprite):
     def __init__(self):
-        super()._init__()
-        self.image = pygame.Surface((50,50))
-        self.image.fill((255,255,255))
+        super(). __init__()
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect()
-        self.rect.center =(400,550)
-        
+        self.rect.center = (400, 550)
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys [pygame.K_LEFT]:
-            self.rect.x-= 5   
-        if keys  [pygame.K_RIGHT]:
-            self.rect.x+= 5
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 5
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 5
 
-class Bullet():
+    
+
+class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.Surface((5,10))
-        self.image.fill((255,0,0))
+        self.image = pygame.Surface((5, 10))
+        self.image.fill((255, 0, 0))
         self.rect = self.image.get_rect()
-        self.rect.center = (x,y)
+        self.rect.center = (x, y)
 
     def update(self):
-        self.rect.y -=5
-        if self.rect.bottom <0:
+        self.rect.y -= 5
+        if self.rect.bottom < 0 :
             self.kill()
 
-class Enemy():
+class Enemy(pygame.sprite.Sprite):  
     def __init__(self):
         super().__init__()
-        self.image.fill(0,255,0)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((0, 255, 0))
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0.750)
-        self.rect.y = random.randint(-200,-50)
+        self.rect.x = random.randint(0, 750)
+        self.rect.y = random.randint(-200, -50)
+
     def update(self):
         self.rect.y +=3
-        if self.rect.top> 600:
-            self.rect.y = random.randint(-100,-40)
-            self.rect.x = random.randint(0,750)
+        if self.rect.top > 600: 
+            self.rect.y = random.randint(-100, -40)
+            self.rect.x = random.randint(0, 750)
+
+
+all_sprites = pygame.sprite.Group()
+
+player = Player()
+
+all_sprites.add(player)
+
+bullets = pygame.sprite.Group()
+
+enemies = pygame.sprite.Group()
+for _ in range(5):
+    enemy = Enemy()
+    all_sprites.add(enemy)
+    enemies.add(enemy)
+
 running = True
 
 while running:
- for event in pygame.event.get():
-      if event.type == pygame.quit:
-         running = False
+    
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+
+    all_sprites.update()
+
 
 pygame.quit()
